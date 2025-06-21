84629affa09dfb119fe66f18f59cbfdb97c7e18c
Karina Trofimova <karina_seidova@mail.ru>
2025-06-21 15:57:09 +0200

    внесены правки по комментариям ревьюера

--- conftest.py
+++ conftest.py
@@ -7,9 +7,9 @@ from praktikum.ingredient_types import INGREDIENT_TYPE_FILLING as FILLING
 @pytest.fixture
 def bun_mock():
     bun_mock = Mock()
-
     bun_mock.name = "Булка"
     bun_mock.price = 150.50
+    bun_mock.get_name.return_value = bun_mock.name
     bun_mock.get_price.return_value = 150.50
     return bun_mock
 
@@ -19,6 +19,8 @@ def ingredient_mock():
     ingredient_mock.name = "Огурчик 'Сила земли'"
     ingredient_mock.price = 50.50
     ingredient_mock.type = random.choice([SAUCE, FILLING])
+    ingredient_mock.get_name.return_value = ingredient_mock.name
+    ingredient_mock.get_type.return_value = ingredient_mock.type
     ingredient_mock.get_price.return_value = 50.50
     return ingredient_mock
 
@@ -29,5 +31,7 @@ def second_ingredient_mock():
     second_ingredient_mock.name = "Соус 'Кровь дракона'"
     second_ingredient_mock.price = 24.50
     second_ingredient_mock.type = random.choice([SAUCE, FILLING])
+    second_ingredient_mock.get_name.return_value = second_ingredient_mock.name
+    second_ingredient_mock.get_type.return_value = second_ingredient_mock.type
     second_ingredient_mock.get_price.return_value = 24.50
     return second_ingredient_mock
--- tests/burger_test.py
+++ tests/burger_test.py
@@ -26,6 +26,14 @@ class TestBurger:
         # Проверяем, что единственный добавленный ингредиент удален
         assert len(burger.ingredients) == 0
 
+    # Проверяем, что ингредиент перемещается на выбранное место и общая длина списка не меняется
+    def test_move_ingredient_move_chosen_ingredient_on_right_slot(self, ingredient_mock, second_ingredient_mock):
+        burger = Burger()
+        burger.add_ingredient(ingredient_mock) # [1_ing]
+        burger.add_ingredient(second_ingredient_mock) # [1_ing, 2_ing]
+        burger.move_ingredient(1, 0) # [2_ing, 1_ing]
+        assert burger.ingredients[0].get_name() == second_ingredient_mock.name and len(burger.ingredients) == 2
+
     # Проверяем подсчет цены
     # Для ингредиентов и булок используем моки
     def test_get_price_return_summ_prices_of_two_buns_and_all_ingredients(self, bun_mock,
@@ -37,9 +45,11 @@ class TestBurger:
         final_price = burger.get_price() # 150.50 * 2 + 50,50 + 24,50 =376,00
         assert final_price == 376,50
 
-"""
-Так как достигнуто требуемое минимальное покрытие?
-a перемещение ингредиентов и печать чека не являются основными фичами,
-на этом тестирование заканчиваем
-Общий процент покрытия 90% - при требуемых 70%
-"""
+    # Проверяем, что в рецепте присутствуют все выбранные ингредиенты
+    def test_get_receipt_print_all_added_ingredients(self, bun_mock, ingredient_mock, second_ingredient_mock):
+        burger = Burger()
+        burger.set_buns(bun_mock)
+        burger.add_ingredient(ingredient_mock)
+        burger.add_ingredient(second_ingredient_mock)
+        receipt = burger.get_receipt()
+        assert bun_mock.name in receipt and ingredient_mock.name in receipt and second_ingredient_mock.name in receipt
